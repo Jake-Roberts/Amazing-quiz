c7c6340482e996791c70400717f3b37de662db5d
JakeRoberts99 <jake.s.roberts99@gmail.com>
2022-01-27 22:32:01 -0700

    added the reset state function and added the showQuestion function as well

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,15 @@ const startButton = document.getElementById("start-btn");
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
-const shuffledQuestions, currentQuestionIndex
-startButton.addEventListener('click', startGame)
+let shuffledQuestions, currentQuestionIndex
 const nextButton = document.getElementById("next-btn");
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+console.log(answerButtonsElement)
 
 
 // create questions object
@@ -53,19 +59,38 @@ function startGame() {
     startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
-    questionContainerElement.classList.remove('hide')
+    questionContainerElement.classList.remove("hide")
     setNextQuestion()
 }
 // next question function 
 function setNextQuestion() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
     
 }
 
+
 function showQuestion(question) {
     questionElement.innerText = question.question
-    
-}
+    question.answers.forEach(answer => {
+      const button = document.createElement('button')
+      button.innerText = answer.text
+      button.classList.add('btn')
+      if (answer.correct) {
+        button.dataset.correct = answer.correct
+      }
+      button.addEventListener('click', selectAnswer)
+      answerButtonsElement.appendChild(button)
+    })
+  }
+
+  function resetState() {
+      nextButton.classList.add('hide')
+      while (answerButtonsElement.firstChild) {
+          answerButtonsElement.removeChild
+          (answerButtonsElement.firstChild)
+      }
+  }
 // highscore function 
 //save to local storage function 
 function saveScore () {
@@ -73,6 +98,6 @@ function saveScore () {
 }
 
 // select answer
-function selectAnswer() {
+function selectAnswer(event) {
 
 }
